e2f63f972e3f43f1f05e0601a682275367bee964
joyoverflow <joyoverflow@sina.com>
2019-01-14 23:01:58 +0800

    增加doudizhu项目

---
+++ doudizhu/src/main/java/pxgd/hyena/com/doudizhu/MultiGameActivity.java
@@ -0,0 +1,13 @@
+package pxgd.hyena.com.doudizhu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_game);
+    }
+}
