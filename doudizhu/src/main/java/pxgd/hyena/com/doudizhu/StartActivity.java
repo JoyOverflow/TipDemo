6cf0e635823e5e17101a069c6af3e4a40a24c19e
joyoverflow <joyoverflow@sina.com>
2019-01-16 23:20:43 +0800

    添加背景音乐

--- doudizhu/src/main/java/pxgd/hyena/com/doudizhu/StartActivity.java
+++ doudizhu/src/main/java/pxgd/hyena/com/doudizhu/StartActivity.java
@@ -1,16 +1,56 @@
 package pxgd.hyena.com.doudizhu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import pxgd.hyena.com.doudizhu.app.MyApplication;
 
 /**
  * 游戏起始界面
  */
 public class StartActivity extends AppCompatActivity {
 
+    private MyApplication app;
+
+    public StartActivity() {
+        app= MyApplication.getInstance();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //播放背景音乐
+        app.playbgMusic("welcome.ogg");
+    }
+
+
+
+    /**
+     * 加载菜单
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    /**
+     * 菜单项被点击
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //播放音乐特效
+        app.play("ok.ogg");
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                break;
+            case R.id.action_exits:
+                break;
+        }
+        return true;
     }
 }
