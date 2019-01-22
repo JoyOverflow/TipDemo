e089e99e3bacd6a3c967e1f8cbf3faa5b2fb26b4
joyoverflow <joyoverflow@sina.com>
2019-01-22 12:09:48 +0800

    新添加模块Account的相关Activity

---
+++ account/src/main/java/pxgd/hyena/com/account/KeyPadActivity.java
@@ -0,0 +1,13 @@
+package pxgd.hyena.com.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KeyPadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_pad);
+    }
+}
