e24baec45f84e7ad2e0db22915ca5db673a4eb39
joyoverflow <joyoverflow@sina.com>
2019-01-30 00:48:29 +0800

    添加回收视图

--- mydemo/src/main/java/pxgd/hyena/com/mydemo/BlankFragment.java
+++ mydemo/src/main/java/pxgd/hyena/com/mydemo/BlankFragment.java
@@ -3,6 +3,7 @@ package pxgd.hyena.com.mydemo;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +23,26 @@ public class BlankFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_blank, container, false);
     }
 
+    private class CrimeHolder extends RecyclerView.ViewHolder{
 
+        public CrimeHolder(View itemView) {
+            super(itemView);
+        }
+    }
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
 
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+
+        }
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
 
 }
